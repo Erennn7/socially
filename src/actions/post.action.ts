7a0251a78c573c3f8d77455dd9a027b7ff9110e4
Samarth Kolarkar <samarthkolarkar7777@gmail.com>
2025-11-07 20:16:32 +0530

    Added auth, db, sidebar,and posts

---
+++ src/actions/post.action.ts
@@ -0,0 +1,29 @@
+import { auth } from "@clerk/nextjs/server";
+import { getDbUserId } from "./user.action";
+import prisma from "@/lib/prisma";
+import { revalidatePath } from "next/cache";
+
+
+export async function createPost(content:string,image:string){
+
+    try { 
+        const userId = await getDbUserId();
+
+        if(!userId) return ;
+
+        const post =await prisma.post.create({
+            data:{
+                content,
+                image,
+                authorId:userId
+            }
+        })
+
+        revalidatePath("/");
+
+        return {success:true, post};
+    }catch(err){
+        console.log("Failed to create post: ",err);
+        
+    }
+}
